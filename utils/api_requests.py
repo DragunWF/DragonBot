b173a7044561c76b5a18bd35f8043a608a094954
DragonWF <plarisanmarc@gmail.com>
2022-01-05 00:40:29 +0800

    Changed bomb coin to bomber coin (BCOIN)

--- utils/api_requests.py
+++ utils/api_requests.py
@@ -23,7 +23,7 @@ def view_nft_values(currency_type=""):
         pvu_url = f"https://api.coingecko.com/api/v3/simple/price?ids=plant-vs-undead-token&vs_currencies={currency_type}"
         bnb_url = f"https://api.coingecko.com/api/v3/simple/price?ids=binancecoin&vs_currencies={currency_type}"
         slp_url = f"https://api.coingecko.com/api/v3/simple/price?ids=smooth-love-potion&vs_currencies={currency_type}"
-        bomb_url = f"https://api.coingecko.com/api/v3/simple/price?ids=bomb&vs_currencies={currency_type}"
+        bomb_url = f"https://api.coingecko.com/api/v3/simple/price?ids=bomber-coin&vs_currencies={currency_type}"
 
         btc_value = requests.get(btc_url, headers={
             'accept': 'application/json'}).json()["bitcoin"][f"{currency_type}"]
@@ -43,7 +43,7 @@ BitCoin (BTC): `{btc_value} {currency}`
 BinanceCoin (BNB): `{bnb_value} {currency}`
 PlantsVsUndead Token (PVU): `{pvu_value} {currency}`
 Smooth Love Potion (SLP): `{slp_value} {currency}`
-Bomb Coin (BOMB): `{bomb_value} {currency}`"""
+Bomber Coin (BCOIN): `{bomb_value} {currency}`"""
     except KeyError:
         responses = ("That's an... Invalid input", "Well that's an invalid input",
                      "You need to enter a valid currency", "Invalid input")
