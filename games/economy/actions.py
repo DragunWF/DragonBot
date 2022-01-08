c352ce57d88934f73e4d0cb21baa8f6ff8e32a14
DragonWF <plarisanmarc@gmail.com>
2022-01-09 02:22:48 +0800

    Added leaderboard, more scavenge responses, bug fixes, optimizations

--- games/economy/actions.py
+++ games/economy/actions.py
@@ -14,46 +14,60 @@ list_of_commands = """
 """
 
 list_of_jobs = """
->>> List of Jobs:
+>>> **List of Jobs:**
 - Farmer
 - Miner
 - Engineer
 """
 
 
-def register(player):
-    with open(data_location, "a") as file:
+def register(player, username):
+    # [id, level, gold, job, inventory, username]
+    with open(data_location, "a", newline="") as file:
         writer = csv.writer(file)
-        writer.writerow([player, 0, 0, "none", []])
-        file.write("\n")
+        writer.writerow([player, 0, 0, "none", [], username])
     return "You have been registered"
 
 
-def work():
-    pass
+def rich_leaderboard():
+    values, output = [], ""
+    with open(data_location, "r", newline="") as file:
+        reader = list(csv.reader(file))
+        reader = list(map(lambda arr: [int(arr[0]), int(
+            arr[1]), int(arr[2]), arr[3], arr[4], arr[5]], reader))
+        for data_set in reader:
+            values.append((data_set[2], data_set[5]))
+        values.sort(reverse=True)
+        placing = 1
+        for data in values:
+            output += f"**{placing}:** `{data[1]}` = **{'{:,}'.format(data[0])} gold**\n"
+            placing += 1
+    return f"""
+>>> **Richest citizens in Dragon's Economy:**
+{output}"""
 
 
 def scavenge(player_data):
     locations = ("caves", "trash cans", "bushes",
                  "Tom's bag", "Tom's destroyed linux machine", "Warcook's house",
-                 "CPT's cave", "shoes", "DJDAN's pillows")
-    gold_gained = random.randint(5, 50)
-    with open(data_location, "r") as data:
+                 "CPT's cave", "shoes", "DJDAN's anime pillows", "dumpsters", "CPT's Chalice")
+    gold_gained = random.randint(5, 25)
+    with open(data_location, "r", newline="") as data:
         data_sets = [x for x in list(csv.reader(data)) if x]
         data_sets = list(map(lambda arr: [int(arr[0]), int(
-            arr[1]), int(arr[2]), arr[3], arr[4]], data_sets))
+            arr[1]), int(arr[2]), arr[3], arr[4], arr[5]], data_sets))
         index = data_sets.index(player_data)
         data_sets[index][2] += gold_gained
-        with open(data_location, "w") as file:
+        with open(data_location, "w", newline="") as file:
             writer = csv.writer(file)
             for row in data_sets:
                 writer.writerow(row)
-    return f"You have scavenged **{gold_gained} gold** from {random.choice(locations)}!"
+    return f"You scavenged **{gold_gained} gold** from {random.choice(locations)}!"
 
 
-def action(command, player_id, option=None):
+def action(command, player_id, username=None, option=None):
     registered = False
-    with open(data_location, "r") as file:
+    with open(data_location, "r", newline="") as file:
         data = [x for x in list(csv.reader(file)) if x]
         for data_set in data:
             if player_id == int(data_set[0]):
@@ -65,26 +79,29 @@ def action(command, player_id, option=None):
 
     if command == "register":
         if not registered:
-            return register(player_id)
+            return register(player_id, username)
         else:
             return "You are already registered"
 
-    if registered:  # The problem here being the fact that the player id turned into a list
+    if registered:
         player_data = None
-        with open(data_location, "r") as file:
+        with open(data_location, "r", newline="") as file:
             reader = list(csv.reader(file))
             for data in reader:
                 if player_id == int(data[0]):
                     player_data = data
                     player_data = [int(player_data[0]), int(player_data[1]),
-                                   int(player_data[2]), player_data[3], player_data[4]]
+                                   int(player_data[2]), player_data[3], player_data[4], player_data[5]]
                     break
 
         if command == "scavenge":
             return scavenge(player_data)
 
         if command == "gold":
-            return f"You have **{player_data[2]} gold**."
+            return f"You have **{'{:,}'.format(player_data[2])} gold**."
+
+        if command == "rich":
+            return rich_leaderboard()
 
         if command == "work":
             pass
