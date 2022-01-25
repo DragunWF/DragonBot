ec56f708a5dc1a9e94d66b0fd5a65a854a6cb6b9
DragonWF <plarisanmarc@gmail.com>
2022-01-25 13:24:23 +0800

    Added Jackbox reward function

--- games/economy/rewards.py
+++ games/economy/rewards.py
@@ -25,4 +25,25 @@ def game_reward(player_id, game):
             writer = csv.writer(data)
             for row in reader:
                 writer.writerow(row)
-        return f"Congrats, You have earned **{gold_reward} gold** from this mini-game!"
+        return f"Congrats, You have earned **{'{:,}'.format(gold_reward)} gold** from this mini-game!"
+
+
+def jackbox_reward(player_id):
+    with open(data_location, "r") as file:
+        reader = [x for x in list(csv.reader(file)) if x]
+        reader = list(map(lambda arr: [int(arr[0]), int(
+            arr[1]), int(arr[2]), arr[3], arr[4], arr[5]], reader))
+        for data_set in reader:
+            if player_id == data_set[0]:
+                index = reader.index(data_set)
+                break
+        else:
+            return "The player you're rewarding either doesn't exist or isn't registered"
+
+        gold_reward = random.randint(2000, 3000)
+        reader[index][2] += gold_reward
+        with open(data_location, "w", newline="") as data:
+            writer = csv.writer(data)
+            for row in reader:
+                writer.writerow(row)
+        return f"Congrats, <@{player_id}>. You have been rewarded with **{'{:,}'.format(gold_reward)}** gold for winning a jackbox game!"
