fdf7db6743f47c5ab34a8e436538d0ae5ac02f6e
DragonWF <plarisanmarc@gmail.com>
2022-01-29 02:01:07 +0800

    Added punishment function

--- games/economy/rewards.py
+++ games/economy/rewards.py
@@ -3,6 +3,7 @@ import csv
 from string import digits
 
 data_location = "data/economy_data.csv"
+# Note for me in the future: Eliminate redundant code
 
 
 def game_reward(player_id, game):
@@ -69,4 +70,25 @@ def custom_reward(player_id, amount):
             writer = csv.writer(data)
             for row in reader:
                 writer.writerow(row)
-        return f"Congrats, <@{player_id}>, you have been generously rewarded with **{'{:,}'.format(amount)}** by the great great DragonWF."
+        return f"Congrats, <@{player_id}>, you have been generously rewarded with **{'{:,}'.format(amount)} gold** by the great great DragonWF."
+
+
+def punish_player(player_id, amount):
+    player_id = int("".join([x for x in player_id if x in digits]))
+
+    with open(data_location, "r") as file:
+        reader = [x for x in list(csv.reader(file)) if x]
+        reader = list(map(lambda arr: [int(arr[0]), int(
+            arr[1]), int(arr[2]), arr[3], arr[4], arr[5]], reader))
+        for data_set in reader:
+            if player_id == data_set[0]:
+                index = reader.index(data_set)
+                break
+        else:
+            return "The player you're punishing either doesn't exist or isn't registered"
+        reader[index][2] -= amount
+        with open(data_location, "w", newline="") as data:
+            writer = csv.writer(data)
+            for row in reader:
+                writer.writerow(row)
+        return f"Congrats, <@{player_id}>, you have been punished and **{'{:,}'.format(amount)} gold** has been taken from you."
