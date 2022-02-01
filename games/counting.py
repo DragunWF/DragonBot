3fcc79a4253fa3b008a0082299b662d3850e3cd1
DragonWF <plarisanmarc@gmail.com>
2022-02-01 12:23:56 +0800

    Fixed event bug

--- games/counting.py
+++ games/counting.py
@@ -15,11 +15,11 @@ def create_counting_id(guild_id, channel_id):
 
         with open(data_location, "a", newline="") as file:
             writer = csv.writer(file)
-            writer.writerow([guild_id, channel_id, "user", "none", 0])
+            writer.writerow([guild_id, channel_id, 123, "none", 0])
         return "Counting channel setup is complete!"
 
 
-def trigger_event(previous_number):
+def trigger_event(previous_number, event_type):
     random_number_1, random_number_2, random_number_3 = random.randint(
         2, 8), random.randint(2, 6), random.randint(1, 4)
     p, x, y, z = previous_number, random_number_1, random_number_2, random_number_3
@@ -27,9 +27,10 @@ def trigger_event(previous_number):
               "addition": (p + x, f"p + {x}"),
               "algebra_1": (p - x + y, f"p - {x} + {y}"),
               "algebra_2": ((p + x) * y, f"(p + {x}) * {y}"),
-              "algebra_3": ((p + x) * (y - z), f"(p + {x}) * ({y} - {z})")}
-    event = random.choice([x for x in events])
-    return events[event]
+              "algebra_3": ((p + x) * (y - z), f"(p + {x}) * ({y} - {z})"),
+              "algebra_4": ((p + x - y) * z, f"(p + {x} - {y}) * {z}")}
+    # event = random.choice([x for x in events])
+    return events[event_type]
 
 
 def check_event():
@@ -52,7 +53,7 @@ def main_counting(guild_id, channel_id, user_id, number):
     with open(data_location, "r") as data:
         reader = list(csv.reader(data))
         reader = list(
-            map(lambda x: [int(x[0]), int(x[1]), x[2], x[3], int(x[4])], reader))
+            map(lambda x: [int(x[0]), int(x[1]), int(x[2]), x[3], int(x[4])], reader))
         for row in reader:
             if row[0] == guild_id and row[1] == channel_id:
                 index = reader.index(row)
@@ -60,20 +61,18 @@ def main_counting(guild_id, channel_id, user_id, number):
         else:
             return
 
-        statement, outputs, equation = "", [], None
-        print(reader[index][3])
-        if reader[index][3] != "event":
+        statement, outputs = "", []
+        if reader[index][3] == "none":
             correct_number = reader[index][4] + 1
         else:
-            event_getter = trigger_event(reader[index][4])
-            correct_number = event_getter[0]
-            equation = event_getter[1]
+            correct_number = trigger_event(
+                reader[index][4], reader[index][3])[0]
             reader[index][3] = "none"
 
         def reset_counting():
             reader[index][4] = 0
             reader[index][3] = "none"
-            reader[index][2] = "user"
+            reader[index][2] = 123
 
         if number == correct_number and user_id != reader[index][2]:
             reader[index][4] = correct_number
@@ -85,19 +84,20 @@ def main_counting(guild_id, channel_id, user_id, number):
             statement = f"WRONG, <@{user_id}> ruined it at **{reader[index][4]}**. The correct number was **{correct_number}**, nerd! Next number is **1**."
             reset_counting()
 
+        outputs.append(statement if statement else True)
+        if check_event() and not statement:
+            events = ("multiplication", "addition",
+                      "algebra_1", "algebra_2", "algebra_3", "algebra_4")
+            reader[index][3] = random.choice(events)
+            equation = trigger_event(reader[index][4], reader[index][3])[1]
+            outputs.append(f"**Event alert!**")
+            outputs.append(
+                f"Next number is the result of the following equation `(p = {reader[index][4]})`:")
+            outputs.append(f"```{equation}```")
+
         with open(data_location, "w", newline="") as file:
             writer = csv.writer(file)
             for row in reader:
                 writer.writerow(row)
-
-        outputs.append(statement if statement else True)
-        if check_event() and not statement:
-            reader[index][3] = "event"
-            outputs.append(f"**Event alert!**")
-            outputs.append(f"""```
-Next number is the result of the following equation (p = {reader[index][4] - 1}):
-{equation}
-```
-""")
 
         return outputs
